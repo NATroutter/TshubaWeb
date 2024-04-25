274d084fe9d68dac7cc077f4ac8358bf273db892
NATroutter <natroutter@outlook.com>
2024-04-25 18:42:43 +0300

    Simplefied

--- web/hosts/main/public_html/index.php
+++ web/hosts/main/public_html/index.php
@@ -1,1 +1,2 @@
-hello from php
+<h4>Hello! this site is hosted in with nginx and php 8.1 using TshubaWeb docker container!</h4>
+<a href="https://github.com/NATroutter/TshubaWeb">https://github.com/NATroutter/TshubaWeb</a>
